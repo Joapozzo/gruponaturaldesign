bcf74fcb4f3c2a93b82a6bd51f6850cd1eac9262
Natural <webmaster@naturalonline.com.ar>
2026-03-06 16:06:06 -0300

    .

--- src/app/components/DesignHero.tsx
+++ src/app/components/DesignHero.tsx
@@ -50,7 +50,7 @@ const DesignHero = () => {
             className="lg:col-span-3 relative h-[50vh] sm:h-[60vh] lg:h-full w-full min-h-[280px]"
           >
             <Image
-              src="/imgs/nosotros3.JPEG"
+              src="/imgs/nosotros3.jpeg"
               alt="Modelos vistiendo uniformes profesionales"
               fill
               className="object-cover"
